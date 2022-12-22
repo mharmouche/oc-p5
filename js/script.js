95c16a590d223b7623c13f1eadbe110bd58d50ec
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2022-12-22 13:00:24 +0100

    script.js
    JS for main page

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let baseURL = "https://azure-brainy-echidna.glitch.me";
+// get all data
+let url =  baseURL + "/api/products";
+var response;
+fetch(url)
+  .then(function(res) {
+    if (res.ok) {
+      	return res.json();
+    }
+  })
+  .then(function(value) {
+	response = value;
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+	
+    	console.log(err);
+  });
+
+var innera = '';
+for (let i in response){
+	//console.log(i);
+	let id = response[i]._id;
+	let imageUrl = response[i].imageUrl;
+	let description = response[i].description;
+	let altTxt = response[i].altTxt;
+	let name = response[i].name;
+	let a ='';
+	a +='<a href="./product.html?id=' + id + '">' + '\n';
+	a +=  '<article>' + '\n';
+	a += '<img src="' + imageUrl +'" alt="'+ altTxt + '">' + 
+
+'\n';
+	a += '<h3 class="productName">' + name +'</h3>' + '\n';
+	a += '<p class="productDescription">' + description + '</p>' 
+
++ '\n';
+	a += '</article> \n </a>' + '\n';
+	console.log(a);
+	innera += a;
+}
+
+
+let items = document.getElementById('items');
+items.innerHTML = innera;
