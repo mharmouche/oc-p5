bbbd8da27a1f48e668277bf02fd728d1c3e0f3fd
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2022-12-23 20:53:03 +0100

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -18,14 +18,17 @@ function initSupprimer(){
 	const deleteItems = document.querySelectorAll('.deleteItem');
 	deleteItems.forEach(deleteItem => {
 		deleteItem.addEventListener('click', function handleClick(event) {
-			console.log('box clicked', event);
+			//console.log('box clicked', event);
 			//deleteItem.setAttribute('style', 'background-color: yellow;');
 			//deleteItem.parentNode.setAttribute('style', 'background-color: orange;');
 			//deleteItem.parentNode.parentNode.setAttribute('style', 'background-color: green;');
 			//deleteItem.parentNode.parentNode.parentNode.setAttribute('style', 'background-color: blue;');
 			//deleteItem.parentNode.parentNode.parentNode.parentNode.setAttribute('style', 'background-color: red;');
 			//deleteItem.parentNode.parentNode.parentNode.parentNode.parentNode.setAttribute('style', 'background-color: cyan;');
-			deleteItem.parentNode.parentNode.parentNode.parentNode.remove() ;
+			let pere = deleteItem.parentNode.parentNode.parentNode.parentNode;
+			console.log("suppression pere " + pere);
+			console.log("id " + pere.data-id);
+			pere.remove() ;
 		});
 	});
 }
