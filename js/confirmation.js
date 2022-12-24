285205a74f56182434e4f196325b655fc4101241
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2022-12-24 16:51:53 +0100

    Update confirmation.js

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,2 +1,19 @@
-orderId = "9c55c670-839b-11ed-a9e9-1fb17e591854";
+// to be organised in externl js and called.
+
+// initialisation : init localStorage
+function init1(){
+  let cart0 = '{"orderId":"","total":0,"kanap":[]}';
+  if (localStorage.cart == null){
+
+      localStorage.setItem( 'cart', cart0);
+
+  };
+  let cart = JSON.parse(localStorage.cart);
+  orderId = cart.orderId;
+  localStorage.setItem( 'cart', cart0);
+}
+// End of file
+orderId = "";
+init1();
+
 document.getElementById("orderId").innerHTML = "<br>" + orderId;
