f70236b369920ba7b88fb60fb532fa4db70abd17
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2022-12-27 00:12:35 +0100

    No refrech confirmation page

--- js/confirmation.js
+++ js/confirmation.js
@@ -11,9 +11,17 @@ function init1(){
   let cart = JSON.parse(localStorage.cart);
   orderId = cart.orderId;
   localStorage.setItem( 'cart', cart0);
+  // identify in case of refrech, bck to main page
+  orderId2 = new URLSearchParams(window.location.search).get('orderId');
+  if ((orderId2 == null) || (orderId !== orderId2))
+  {
+    window.location.href = `./index.html`;
+  }
+ 
 }
 // End of file
 orderId = "";
 init1();
 
 document.getElementById("orderId").innerHTML = "<br>" + orderId;
+
